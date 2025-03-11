fd1e2a911defe43121e37795d15526c802c908d4
LinCie <aldiofernanda@protonmail.com>
2025-03-11 17:14:29 +0700

    feat: add basic theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sistem Kelas',
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xff88c0d0),
+          onPrimary: Color(0xffeceff4),
+          secondary: Color(0xff81a1c1),
+          onSecondary: Color(0xffeceff4),
+          error: Color(0xffbf616a),
+          onError: Color(0xffeceff4),
+          surface: Color(0xffeceff4),
+          onSurface: Color(0xff2e3440),
+        ),
+      ),
       home: const HomePage(title: "Sistem Kelas"),
     );
   }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -30,18 +30,55 @@ class AppDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
+    void onTileTap() {
+      Navigator.pop(context);
+    }
+
     return Drawer(
       child: ListView(
-        padding: EdgeInsets.zero,
+        padding: EdgeInsets.all(0),
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [theme.primaryColor]),
+          SizedBox(
+            height: 200,
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    theme.colorScheme.primary,
+                    theme.colorScheme.secondary,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: theme.colorScheme.surface,
+                  ),
+                  SizedBox(height: 10),
+                  Text("John Doe", style: theme.textTheme.headlineSmall),
+                  Text("@username", style: theme.textTheme.labelMedium),
+                ],
+              ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [CircleAvatar(radius: 40)],
-            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text("Class"),
+            onTap: onTileTap,
+          ),
+          ListTile(
+            leading: Icon(Icons.schedule),
+            title: Text("Schedule"),
+            onTap: onTileTap,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Profile"),
+            onTap: onTileTap,
           ),
         ],
       ),
